f4c638e55664f3944cb9d0b82f8216a9a38a3e04
kvd <vodangkhoangute@gmail.com>
2024-10-02 10:55:35 +0000

    feat: add file to deploy project

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:khoang-dev/khoang-dev.github.io.git master:deploy
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/khoang-dev/portfolio.git master:deploy
+
+cd -
